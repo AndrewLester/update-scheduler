71790859f338c3b7636e3198db00f3de671fec86
AndrewLester <alester220@gmail.com>
2021-03-24 17:06:42 -0400

    Add new dependencies

--- snowpack.config.js
+++ snowpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
             plugins: [
                 require('rollup-plugin-scss')()
             ]
-        }
+        },
+        polyfillNode: true
     },
     plugins: [
         '@snowpack/plugin-svelte',
